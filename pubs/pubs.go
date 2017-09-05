d713fb95c2f102e384f2009004ac48f29af3264f
Martin Garton <mgarton@utilitywarehouse.co.uk>
2017-09-05 15:57:35 +0100

    Added uri based message sources
    Added url based message sources, to enable switching to different
    backends without application changes.
    
    This is an experimental poc, so not much is implemented yet.

---
+++ pubs/pubs.go
@@ -0,0 +1,89 @@
+package pubs
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"strings"
+	"time"
+
+	pubsub "github.com/utilitywarehouse/go-pubsub"
+	"github.com/utilitywarehouse/go-pubsub/kafka"
+	"github.com/utilitywarehouse/go-pubsub/natss"
+)
+
+// NewSource will return a message source based on the supplied URL.
+// Examples:
+//  kafka://localhost:123/my-topic/?offset=latest;consumer-group=g1;metadata-refresh=2s;broker=localhost:234;broker=localhost:345
+//  nats-streaming://localhost:123/my-topic?cluster-id=cid-1;consumer-id=cons-1
+func NewSource(uri string) (pubsub.MessageSource, error) {
+	parsed, err := url.Parse(uri)
+	if err != nil {
+		return nil, err
+	}
+	switch parsed.Scheme {
+	case "kafka":
+		return newKafka(parsed)
+	case "nats-streaming":
+		return newNatsStreaming(parsed)
+	default:
+		return nil, fmt.Errorf("unknown scheme : %s", parsed.Scheme)
+	}
+}
+
+func newKafka(uri *url.URL) (pubsub.MessageSource, error) {
+	q := uri.Query()
+
+	topic := strings.Trim(uri.Path, "/")
+
+	if strings.Contains(topic, "/") {
+		return nil, fmt.Errorf("error parsing topic from url (%s)", topic)
+	}
+
+	conf := kafka.MessageSourceConfig{
+		Brokers:       []string{uri.Host},
+		ConsumerGroup: q.Get("consumer-group"),
+		Topic:         topic,
+	}
+
+	conf.Brokers = append(conf.Brokers, q["broker"]...)
+
+	switch q.Get("offset") {
+	case "latest":
+		conf.Offset = kafka.OffsetLatest
+	case "oldest":
+		conf.Offset = kafka.OffsetLatest
+	case "":
+	default:
+		log.Printf("ignoring unknown offset value '%s'\n", q.Get("offset"))
+	}
+
+	dur := q.Get("metadata-refresh")
+	if dur != "" {
+		d, err := time.ParseDuration(dur)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse refresh duration : %v", err)
+		}
+		conf.MetadataRefreshFrequency = d
+	}
+
+	return kafkaSourcer(conf), nil
+}
+
+var kafkaSourcer = kafka.NewMessageSource
+
+func newNatsStreaming(uri *url.URL) (pubsub.MessageSource, error) {
+	q := uri.Query()
+
+	natsURL := "nats://" + uri.Host
+
+	topic := strings.Trim(uri.Path, "/")
+	if strings.Contains(topic, "/") {
+		return nil, fmt.Errorf("error parsing topic from url (%s)", topic)
+	}
+
+	return natsStreamingSourcer(natsURL, q.Get("cluster-id"), q.Get("consumer-id"), topic)
+
+}
+
+var natsStreamingSourcer = natss.NewMessageSource
---
+++ pubs/pubs_test.go
@@ -0,0 +1,159 @@
+package pubs
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	pubsub "github.com/utilitywarehouse/go-pubsub"
+	"github.com/utilitywarehouse/go-pubsub/kafka"
+)
+
+func TestKafka(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		name        string
+		input       string
+		expected    kafka.MessageSourceConfig
+		expectedErr error
+	}{
+		{
+			name:  "simple",
+			input: "kafka://localhost",
+			expected: kafka.MessageSourceConfig{
+				Brokers: []string{"localhost"},
+			},
+			expectedErr: nil,
+		},
+		{
+			name:  "standard",
+			input: "kafka://localhost:123/t1",
+			expected: kafka.MessageSourceConfig{
+				Brokers: []string{"localhost:123"},
+				Topic:   "t1",
+			},
+			expectedErr: nil,
+		},
+		{
+			name:  "with-port",
+			input: "kafka://localhost:123/t1",
+			expected: kafka.MessageSourceConfig{
+				Brokers: []string{"localhost:123"},
+				Topic:   "t1",
+			},
+			expectedErr: nil,
+		},
+		{
+			name:  "everything",
+			input: "kafka://localhost:123/t1/?offset=latest;consumer-group=g1;metadata-refresh=2s;broker=localhost:234;broker=localhost:345",
+			expected: kafka.MessageSourceConfig{
+				Brokers:                  []string{"localhost:123", "localhost:234", "localhost:345"},
+				ConsumerGroup:            "g1",
+				MetadataRefreshFrequency: 2 * time.Second,
+				Offset: kafka.OffsetLatest,
+				Topic:  "t1",
+			},
+			expectedErr: nil,
+		},
+	}
+
+	for _, tst := range tests {
+		t.Run(tst.name, func(t *testing.T) {
+
+			var conf kafka.MessageSourceConfig
+			kafkaSourcer = func(c kafka.MessageSourceConfig) pubsub.MessageSource {
+				conf = c
+				return nil
+			}
+			_, err := NewSource(tst.input)
+
+			if tst.expectedErr != err {
+				t.Errorf("expected error %v but got %v", tst.expectedErr, err)
+			}
+
+			assert.Equal(tst.expected, conf)
+		})
+	}
+
+}
+
+func TestNatsStreaming(t *testing.T) {
+	assert := assert.New(t)
+
+	type natsConf struct{ natsURL, clusterID, consumerID, topic string }
+
+	tests := []struct {
+		name        string
+		input       string
+		expected    natsConf
+		expectedErr bool
+	}{
+		{
+			name:  "simple",
+			input: "nats-streaming://localhost/t1",
+			expected: natsConf{
+				natsURL: "nats://localhost",
+				topic:   "t1",
+			},
+			expectedErr: false,
+		},
+		{
+			name:  "simple-trailing-slash",
+			input: "nats-streaming://localhost/t1/",
+			expected: natsConf{
+				natsURL: "nats://localhost",
+				topic:   "t1",
+			},
+			expectedErr: false,
+		},
+		{
+			name:  "with-port",
+			input: "nats-streaming://localhost:123/t1",
+			expected: natsConf{
+				natsURL: "nats://localhost:123",
+				topic:   "t1",
+			},
+			expectedErr: false,
+		},
+		{
+			name:  "everything",
+			input: "nats-streaming://localhost:123/t1?cluster-id=cid-1;consumer-id=cons-1",
+			expected: natsConf{
+				natsURL:    "nats://localhost:123",
+				clusterID:  "cid-1",
+				consumerID: "cons-1",
+				topic:      "t1",
+			},
+			expectedErr: false,
+		},
+		{
+			name:        "extra-path-elements",
+			input:       "nats-streaming://localhost:123/aa/bb",
+			expected:    natsConf{},
+			expectedErr: true,
+		},
+	}
+
+	for _, tst := range tests {
+		t.Run(tst.name, func(t *testing.T) {
+
+			var conf natsConf
+			natsStreamingSourcer = func(natsURL, clusterID, consumerID, topic string) (pubsub.MessageSource, error) {
+				conf.natsURL = natsURL
+				conf.clusterID = clusterID
+				conf.consumerID = consumerID
+				conf.topic = topic
+				return nil, nil
+			}
+			_, err := NewSource(tst.input)
+
+			if tst.expectedErr == (err == nil) {
+				t.Errorf("expected error %v but got %v", tst.expectedErr, err)
+			}
+
+			assert.Equal(tst.expected, conf)
+		})
+	}
+
+}
