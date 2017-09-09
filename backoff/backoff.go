3e6cf95e618cb42d049c2a87c91f735efabd89f6
jakekeeys <jake@keeys.org>
2017-09-09 15:13:59 +0100

    adds ExponentialBackOffRetryingErrorHandler

---
+++ backoff/backoff.go
@@ -0,0 +1,35 @@
+package backoff
+
+import (
+	"time"
+	"math"
+	"github.com/utilitywarehouse/go-pubsub"
+)
+
+type ExponentialBackOffRetryingErrorHandler pubsub.ConsumerErrorHandler
+
+func New(handler pubsub.ConsumerMessageHandler) ExponentialBackOffRetryingErrorHandler {
+	return func(msg pubsub.ConsumerMessage, err error) error {
+		for i := 0; i < 3; i++ {
+			err := handler(msg)
+			if err == nil {
+				return nil
+			}
+			<-time.After(time.Duration(3*math.Pow(2, float64(i+1))) * time.Second)
+		}
+		return err
+	}
+}
+
+func NewWithFallBack(handler pubsub.ConsumerMessageHandler, errHandler pubsub.ConsumerErrorHandler) ExponentialBackOffRetryingErrorHandler {
+	return func(msg pubsub.ConsumerMessage, err error) error {
+		for i := 0; i < 3; i++ {
+			err := handler(msg)
+			if err == nil {
+				return nil
+			}
+			<-time.After(time.Duration(3*math.Pow(2, float64(i+1))) * time.Second)
+		}
+		return errHandler(msg, err)
+	}
+}
