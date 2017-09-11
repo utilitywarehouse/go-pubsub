9fc5edbb642ad887960419c2c6c421b7eec4ed72
jakekeeys <jake@keeys.org>
2017-09-11 11:27:59 +0100

    make retries configurable

--- backoff/backoff.go
+++ backoff/backoff.go
@@ -8,15 +8,19 @@ import (
 
 type ExponentialBackOffRetryingErrorHandler pubsub.ConsumerErrorHandler
 
-func New(handler pubsub.ConsumerMessageHandler) ExponentialBackOffRetryingErrorHandler {
-	return NewWithFallBack(handler, func(msg pubsub.ConsumerMessage, err error) error {
-		return err
-	})
+func New(handler pubsub.ConsumerMessageHandler, retries int) ExponentialBackOffRetryingErrorHandler {
+	return NewWithFallBack(
+		handler,
+		func(msg pubsub.ConsumerMessage, err error) error {
+			return err
+		},
+		retries,
+	)
 }
 
-func NewWithFallBack(handler pubsub.ConsumerMessageHandler, errHandler pubsub.ConsumerErrorHandler) ExponentialBackOffRetryingErrorHandler {
+func NewWithFallBack(handler pubsub.ConsumerMessageHandler, errHandler pubsub.ConsumerErrorHandler, retries int) ExponentialBackOffRetryingErrorHandler {
 	return func(msg pubsub.ConsumerMessage, err error) error {
-		for i := 0; i < 3; i++ {
+		for i := 0; i < retries; i++ {
 			err := handler(msg)
 			if err == nil {
 				return nil
