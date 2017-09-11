d89d655db1e6d967ab851f9cdf7c4141ab417d4c
jakekeeys <jake@keeys.org>
2017-09-11 11:29:09 +0100

    allow a message topic to be passed and included in the FailedConsumerMessage

--- deadletter/deadletter.go
+++ deadletter/deadletter.go
@@ -12,20 +12,26 @@ type DeadLetteringErrorHandler pubsub.ConsumerErrorHandler
 type FailedConsumerMessage struct {
 	Message       pubsub.ConsumerMessage `json:"message"`
 	MessageBase64 string                 `json:"messageBase64"`
+	MessageTopic  string                 `json:"messageTopic"`
 	Err           error                  `json:"error"`
 }
 
-func New(sink pubsub.MessageSink) DeadLetteringErrorHandler {
-	return NewWithFallback(sink, func(msg pubsub.ConsumerMessage, err error) error {
-		return err
-	})
+func New(sink pubsub.MessageSink, messageTopic string) DeadLetteringErrorHandler {
+	return NewWithFallback(
+		sink,
+		func(msg pubsub.ConsumerMessage, err error) error {
+			return err
+		},
+		messageTopic,
+	)
 }
 
-func NewWithFallback(sink pubsub.MessageSink, errHandler pubsub.ConsumerErrorHandler) DeadLetteringErrorHandler {
+func NewWithFallback(sink pubsub.MessageSink, errHandler pubsub.ConsumerErrorHandler, messageTopic string) DeadLetteringErrorHandler {
 	return func(msg pubsub.ConsumerMessage, err error) error {
 		failedMsg := FailedConsumerMessage{
 			Message:       msg,
 			MessageBase64: base64.StdEncoding.EncodeToString(msg.Data),
+			MessageTopic:  messageTopic,
 			Err:           err,
 		}
 		failedMsgJSON, err := json.Marshal(failedMsg)
