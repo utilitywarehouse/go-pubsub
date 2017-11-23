ace64a09d793a4b4126906f3e57431876bdf53c5
George Gkirtsou <ggirtsou@utilitywarehouse.co.uk>
2017-11-23 17:27:13 +0000

    Update example that was SNS specific

--- sqs/example/source.go
+++ sqs/example/source.go
@@ -2,29 +2,11 @@ package example
 
 import (
 	"context"
-	"encoding/json"
 	"log"
-	"time"
 
-	"github.com/pkg/errors"
 	pubsub "github.com/utilitywarehouse/go-pubsub"
 	"github.com/utilitywarehouse/go-pubsub/sqs"
 )
-
-// Message struct represents an SQS message with json tags in order to unmarshal it.
-// Make sure struct fields are exported so unmarshalling can assign values.
-type Message struct {
-	Type             string     `json:"Type"`
-	MessageID        string     `json:"MessageId"`
-	TopicArn         string     `json:"TopicArn"`
-	Subject          string     `json:"Subject"`
-	Message          string     `json:"Message"`
-	Timestamp        *time.Time `json:"Timestamp"`
-	SignatureVersion string     `json:"SignatureVersion"`
-	Signature        string     `json:"Signature"`
-	SigningCertURL   string     `json:"SigningCertURL"`
-	UnsubscribeURL   string     `json:"UnsubscribeURL"`
-}
 
 func consumerExample() {
 	source, err := sqs.NewMessageSource(sqs.MessageSourceConfig{
@@ -50,11 +32,6 @@ func consumerExample() {
 
 // msgHandler will process messages from SQS.
 func msgHandler(msg pubsub.ConsumerMessage) error {
-	var sqsMsg Message
-	if err := json.Unmarshal(msg.Data, sqsMsg); err != nil {
-		return errors.Wrap(err, "failed to unmarshal SQS message")
-	}
-
 	// Add your message handling logic here.
 
 	return nil
