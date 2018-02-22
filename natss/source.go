b5393e7e79c33d0b7ea4cd784ff892d27fa03959
ricky00122@googlemail.com <ricky00122@googlemail.com>
2018-02-22 12:05:04 +0000

    offset allowed

--- natss/source.go
+++ natss/source.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"errors"
+	"time"
 
 	"github.com/nats-io/go-nats-streaming"
 	"github.com/nats-io/go-nats-streaming/pb"
@@ -23,18 +24,33 @@ func generateID() string {
 var _ pubsub.MessageSource = (*messageSource)(nil)
 
 type MessageSourceConfig struct {
-	NatsURL    string
-	ClusterID  string
-	Topic      string
-	ConsumerID string
+	NatsURL             string
+	ClusterID           string
+	Topic               string
+	ConsumerID          string
+	Offset              Offset
+	OffsetStartAtIndex  uint64
+	OffsetStartDuration time.Duration
 }
 
+type Offset int
+
 type messageSource struct {
-	natsURL    string
-	clusterID  string
-	consumerID string
-	topic      string
+	natsURL             string
+	clusterID           string
+	consumerID          string
+	topic               string
+	offset              Offset
+	offsetStartAtIndex  uint64
+	offsetStartDuration time.Duration
 }
+
+const (
+	OFFSET_START_AT     Offset = iota
+	OFFSET_DELIVER_LAST Offset = iota
+	OFFSET_DELIVER_ALL  Offset = iota
+	OFFSET_START_TIME   Offset = iota
+)
 
 func NewMessageSource(conf MessageSourceConfig) (pubsub.MessageSource, error) {
 	return &messageSource{
@@ -77,7 +93,23 @@ func (mq *messageSource) ConsumeMessages(ctx context.Context, handler pubsub.Con
 		}
 	}
 
+	//defalt to start from first index
 	startOpt := stan.StartAt(pb.StartPosition_First)
+
+	switch offset := mq.offset; offset {
+
+	case OFFSET_START_AT:
+		startOpt = stan.StartAtSequence(mq.offsetStartAtIndex)
+
+	case OFFSET_DELIVER_LAST:
+		startOpt = stan.StartWithLastReceived()
+
+	case OFFSET_DELIVER_ALL:
+		startOpt = stan.DeliverAllAvailable()
+
+	case OFFSET_START_TIME:
+		startOpt = stan.StartAtTimeDelta(mq.offsetStartDuration)
+	}
 
 	subcription, err := conn.QueueSubscribe(mq.topic, mq.consumerID, f, startOpt, stan.DurableName(mq.consumerID), stan.SetManualAckMode())
 
