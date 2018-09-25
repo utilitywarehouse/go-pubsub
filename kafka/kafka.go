fcf8bf091e5fcd5d9bbaf3fab501c41ae6adab77
Daniel Copaciu <dani.copaciu@gmail.com>
2018-09-25 10:06:50 +0100

    Disable metrics by default

--- kafka/kafka.go
+++ kafka/kafka.go
@@ -2,7 +2,6 @@ package kafka
 
 import metrics "github.com/rcrowley/go-metrics"
 
-// InternalMetrics enables or disables sarama internal metrics
-func InternalMetrics(enabled bool) {
-	metrics.UseNilMetrics = !enabled
+func init() {
+	metrics.UseNilMetrics = true
 }
