6e37916c3161d88ee24a75239d72b28d17ebf887
denislobanov <denis.lobanov@zooei.com>
2018-12-11 15:43:22 +0600

    Clear last error in case of temporary issues
    RWMutex around last error to ensure package is thread safe.

--- sns/sink.go
+++ sns/sink.go
@@ -1,6 +1,8 @@
 package sns
 
 import (
+	"sync"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
@@ -10,9 +12,14 @@ import (
 )
 
 type messageSink struct {
-	client    snsiface.SNSAPI
-	topic     string
-	lastError error
+	client snsiface.SNSAPI
+	topic  string
+	state  sinkState
+}
+
+type sinkState struct {
+	sync.RWMutex
+	err error
 }
 
 // NewSNSSink is a constructor for new AWS SNS MessageSink type
@@ -23,17 +30,19 @@ func NewSNSSink(conf *aws.Config, topic string) (pubsub.MessageSink, error) {
 	}
 
 	return &messageSink{
-		client:    sns.New(sess),
-		topic:     topic,
-		lastError: nil,
+		client: sns.New(sess),
+		topic:  topic,
 	}, nil
 }
 
 // PutMessage sends ProducerMessage types to AWS SNS
 func (s *messageSink) PutMessage(message pubsub.ProducerMessage) error {
+	// mark status as healthy in case of temporary issue
+	s.lastErr(nil)
+
 	b, err := message.Marshal()
 	if err != nil {
-		s.lastError = err
+		s.lastErr(err)
 		return errors.Wrap(err, "SNS Sink could not marshal ProducerMessage")
 	}
 
@@ -44,7 +53,7 @@ func (s *messageSink) PutMessage(message pubsub.ProducerMessage) error {
 
 	_, err = s.client.Publish(input)
 	if err != nil {
-		s.lastError = err
+		s.lastErr(err)
 		return errors.Wrap(err, "error publishing to SNS")
 	}
 
@@ -56,9 +65,12 @@ func (s *messageSink) Status() (*pubsub.Status, error) {
 	status := &pubsub.Status{
 		Working: true,
 	}
-	if s.lastError != nil {
+
+	s.state.RLock()
+	defer s.state.RUnlock()
+	if s.state.err != nil {
 		status.Working = false
-		status.Problems = append(status.Problems, s.lastError.Error())
+		status.Problems = append(status.Problems, s.state.err.Error())
 	}
 
 	return status, nil
@@ -67,4 +79,10 @@ func (s *messageSink) Status() (*pubsub.Status, error) {
 // Close is stubbed as there is no equivalent on the SNS Client.
 func (s *messageSink) Close() error {
 	return nil
+}
+
+func (s *messageSink) lastErr(err error) {
+	s.state.Lock()
+	s.state.err = err
+	s.state.Unlock()
 }
